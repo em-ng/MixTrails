63f4e664e65c4a97057ebd580ba73ea06d32e99a
Emily <myng.emily@gmail.com>
2021-12-31 23:35:28 -0500

    Created users and session ctrllr

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,2 +1,17 @@
 class Api::SessionsController < ApplicationController
+    def create
+        @user = User.find_by_credentials(params[:user][:email], params[:user][:password])
+
+        if @user
+            login!(@user)
+            render '/api/users/show'
+        else
+            render json: ["Login failed. Please try again."], status: 422
+        end
+    end
+
+    def destroy
+        logout!
+        render json: {}
+    end
 end
