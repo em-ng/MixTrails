d93316627008ac96081c108ca53a71fba187cfbd
Emily <myng.emily@gmail.com>
2022-01-01 18:19:23 -0500

    Created store and added to entry file

--- frontend/mix_trails.jsx
+++ frontend/mix_trails.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as SessionAPIUtil from './util/session_api_util';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
+  const store = configureStore();
 
   //window testing-delete later
   window.login = SessionAPIUtil.login;
   window.logout = SessionAPIUtil.logout;
   window.signUp = SessionAPIUtil.signUp;
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
 
 
   ReactDOM.render(<h1>MixTrails</h1>, root);
