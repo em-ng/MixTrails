5290fee4f2b2a439b33f22a1ffc9e7eb20100f84
Emily <myng.emily@gmail.com>
2022-01-01 17:39:09 -0500

    Created session errors reducer

---
+++ frontend/reducers/session_errors_reducer.js
@@ -0,0 +1,19 @@
+import { RECEIVE_CURRENT_USER, RECEIVE_SESSION_ERRORS } from "../actions/session_actions";
+import sessionReducer from "./session_reducer";
+
+const sessionErrorsReducer = (state = [], action) => {
+    Object.freeze(state)
+
+    switch (action.type) {
+        case RECEIVE_SESSION_ERRORS:
+            return action.errors;
+            
+        case RECEIVE_CURRENT_USER:
+            return [];
+    
+        default:
+            return state;
+    }
+}
+
+export default sessionErrorsReducer;
