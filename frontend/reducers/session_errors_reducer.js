9b03000f2e4936d7e056da3600c24ae4ab6942db
Emily <myng.emily@gmail.com>
2022-01-01 20:30:49 -0500

    Added clear errors

--- frontend/reducers/session_errors_reducer.js
+++ frontend/reducers/session_errors_reducer.js
@@ -1,5 +1,4 @@
-import { RECEIVE_CURRENT_USER, RECEIVE_SESSION_ERRORS } from "../actions/session_actions";
-import sessionReducer from "./session_reducer";
+import { RECEIVE_CURRENT_USER, RECEIVE_SESSION_ERRORS, CLEAR_ERRORS } from "../actions/session_actions";
 
 const sessionErrorsReducer = (state = [], action) => {
     Object.freeze(state)
@@ -7,6 +6,9 @@ const sessionErrorsReducer = (state = [], action) => {
     switch (action.type) {
         case RECEIVE_SESSION_ERRORS:
             return action.errors;
+
+        case CLEAR_ERRORS:
+            return [];
             
         case RECEIVE_CURRENT_USER:
             return [];
