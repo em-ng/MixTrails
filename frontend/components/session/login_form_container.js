aae8eebef705631e321c20b9388d4d85e00e2b8c
Emily <myng.emily@gmail.com>
2022-01-02 00:15:56 -0500

    Created login form container

---
+++ frontend/components/session/login_form_container.js
@@ -0,0 +1,23 @@
+import { connect } from 'react-redux';
+import { login } from '../../actions/session_actions';
+import SessionForm from './session_form';
+
+const mSTP = state => {
+    return {
+        formHeader: "Log in and let's get going",
+        formType: 'Log in',
+        nullUser: {
+            email: '',
+            password: ''
+        },
+        errors: state.errors.session
+    }
+}
+
+const mDTP = dispatch => {
+    return {
+        processForm: user => dispatch(login(user))
+    }
+}
+
+export default connect(mSTP, mDTP)(SessionForm);
