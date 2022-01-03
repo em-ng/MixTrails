baac57fc2b9c788e407d2bb379ed80567af3523f
Emily <myng.emily@gmail.com>
2022-01-03 16:57:37 -0500

    Added protected route util

--- frontend/util/route_util.jsx
+++ frontend/util/route_util.jsx
@@ -2,12 +2,6 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Redirect, Route, withRouter} from 'react-router-dom';
 
-const mSTP = state => {
-    return {
-        loggedIn: Boolean(state.session.currentUserId)
-    }
-}
-
 const Auth = ({ loggedIn, path, component: Component, exact }) => {
     return (
         <Route
@@ -20,4 +14,23 @@ const Auth = ({ loggedIn, path, component: Component, exact }) => {
     )
 }
 
+const Protected = ({ loggedIn, path, component: Component, exact }) => {
+    return (
+        <Route
+          path={path}
+          exact={exact}
+          render={props => (
+              loggedIn ? <Component {...props} /> : <Redirect to='/login' />
+          )}
+        />
+    )
+}
+
+const mSTP = state => {
+    return {
+        loggedIn: Boolean(state.session.currentUserId)
+    }
+}
+
 export const AuthRoute = withRouter(connect(mSTP)(Auth));
+export const ProtectedRoute = withRouter(connect(mSTP)(Protected));
