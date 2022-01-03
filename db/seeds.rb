c3deee0256d9c286fd9b1c3c7bc9d7467757901f
Emily <myng.emily@gmail.com>
2022-01-03 17:50:19 -0500

    Seeded demo user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+demo = User.create(firstname: 'Willow', lastname: 'Granger', email: 'willow@granger.com', password: 'hogwarts')
+user1 = User.create(firstname: 'Harry', lastname: 'Potter', email: 'harry@potter.com', password: 'hogwarts')
+user2 = User.create(firstname: 'Ron', lastname: 'Weasley', email: 'ron@weasley.com', password: 'hogwarts')
