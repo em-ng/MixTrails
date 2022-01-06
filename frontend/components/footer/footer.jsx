e01e1e33c56730ae472d57382f125b2789b17c53
Emily <myng.emily@gmail.com>
2022-01-05 23:14:38 -0500

    Finish styling footer

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+
+library.add(fab)
 
 const Footer = () => {
     return(
         <footer>
             <div className="footer-box">
-                <div className="footer-logo">
-                    <Link to='/'>
-                        <img src={window.logo_url}></img>
-                        <span>MixTrails</span>
+                <div>
+                    <Link className="footer-logo" to='/'>
+                        <img className="footer-icon" src={window.logo_url}></img>
+                        <span className="footer-text">MixTrails</span>
                     </Link>
                 </div>
 
                 <div className="footer-links">
-                    <a href="https://github.com/em-ng"><i class="fab fa-github-alt"></i></a>
-                    <a href=""><i class="fab fa-linkedin-in"></i></a>
+                    <a className="icon" href="https://github.com/em-ng"><FontAwesomeIcon icon={["fab", "github-alt"]} /></a>
+                    <a className="icon" href="https://www.linkedin.com/in/emily-n-8a9822204/"><FontAwesomeIcon icon={["fab", "linkedin-in"]} /></a>
                 </div>
             </div>
         </footer>
