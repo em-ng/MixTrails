21ce3c2ad455e5d845ff0033c5c8f2ddcccbf789
Emily <myng.emily@gmail.com>
2022-01-13 01:33:40 -0500

    Google maps rendering on trail show page

--- frontend/components/maps/map.jsx
+++ frontend/components/maps/map.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+// import ReactDOM from 'react-dom';
 
 class Map extends React.Component {
     constructor(props) {
@@ -13,35 +14,48 @@ class Map extends React.Component {
             center = { lat: this.props.trail.lat, lng: this.props.trail.long };
         }
 
+        // const mapNode = ReactDOM.findDOMNode(this.map);  //this.refs.map deprecated?
+
         const mapOptions = {
           center: center,
           zoom: 8
         };
     
         // wrap this.mapNode in a Google Map
-        this.map = new google.maps.Map(this.mapNode, mapOptions);
+        this.map = new google.maps.Map(this.mapNode, mapOptions); //this.mapNode commented out and still works, why...
         // ------- above creates the outer map without marker
 
-        if (this.props.park) {
-            mark = new google.maps.Marker({position: {lat: this.props.park.lat, lng: this.props.park.long}}),
-            mark.setMap(this.map);
+        const pos1 = new google.maps.LatLng(this.props.trail.lat, this.props.trail.long);
+        const pos2 = new google.maps.LatLng(this.props.park.lat, this.props.park.long);
+
+        let marker;
+
+        if (this.props.trail) {
+            marker = new google.maps.Marker({
+                position: pos1,
+                map: this.map
+            });
         } else {
-            mark = new google.maps.Marker({position: {lat: this.props.trail.lat, lng: this.props.trail.long}}),
-            mark.setMap(this.map);
+            marker = new google.maps.Marker({
+                position: pos2,
+                map: this.map
+            });
         }
+
+        // if (this.props.park) {
+        //     marker = new google.maps.Marker({position: {lat: this.props.park.lat, lng: this.props.park.long}}),
+        //     marker.setMap(this.map);
+        // } else {
+        //     marker = new google.maps.Marker({position: {lat: this.props.trail.lat, lng: this.props.trail.long}}),
+        //     marker.setMap(this.map);
+        // }
     }
 
     render() {
         debugger
-
-        if (this.props.park) {
-            mark = new google.maps.Marker({position: {lat: this.props.park.lat, lng: this.props.park.long}}).setMap(this.map);
-        } else {
-            mark = new google.maps.Marker({position: {lat: this.props.trail.lat, lng: this.props.trail.long}}).setMap(this.map);
-        }
-
+        
         return(
-            <div className="map" ref="map">
+            <div className="map" ref={ map => this.mapNode = map }>
     
             </div>
         )
